6b912682264a1723436506feef5b03f236c3ad2f
systemilc <isaac.systemilc@gmail.com>
2022-07-24 12:15:41 -0300

    inserido url localhost app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import Dicas from "./src/controllers/Dicas.js";
 dotenv.config();
 
 const port =  process.env.PORT || 3000;
-const url = 'http://127.0.0.1'
+const url = 'http://localhost'
  const app = express()
 
  app.listen(port, ()=>{
